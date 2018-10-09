dd3b4ce34d6e732d4def688e6c6c740a24c92a67
Brian Zelip <bzelip@gmail.com>
2018-10-09 19:30:05 -0400

    add TheBaseLayout for the main app view

---
+++ src/components/TheBaseLayout.vue
@@ -0,0 +1,23 @@
+<template lang="pug">
+  body.p1.sm-p2.md-p3.bg-texture.flex.flex-column
+    TheHeader
+    TheGroceryList
+    TheFooter
+</template>
+
+<script>
+import TheHeader from './TheHeader.vue';
+import TheGroceryList from './TheGroceryList.vue';
+import TheFooter from './TheFooter.vue';
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    TheHeader,
+    TheGroceryList,
+    TheFooter
+  }
+};
+</script>
