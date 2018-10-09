083e0c2e9edcdb69f6762002da504be0f67f2a6c
Brian Zelip <bzelip@gmail.com>
2018-10-09 18:48:03 -0400

    add footer markup plus change its file name via https://vuejs.org/v2/style-guide/\#Single-instance-component-names-strongly-recommended

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+  footer.flex.flex-column.flex-auto.justify-end.mt3
+    .flex.justify-center
+      p.blue by 
+        a(href="http://zelip.me") bzelip
+        span.purp  | 
+        a(href="https://github.com/brianzelip/groceries") github
+        span.purp  | 
+        a(href="https://glitch.com/~romantic-study") glitch
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
