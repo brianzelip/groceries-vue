5b822e949df93fc11ff9fed68095a2c449ae2756
Brian Zelip <bzelip@gmail.com>
2018-10-10 13:06:33 -0400

    init veux in app.js

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 import Vue from 'vue';
+import Vuex from 'vuex';
 import VueResource from 'vue-resource';
 import App from './src/App.vue';
 
+Vue.use(Vuex);
 Vue.use(VueResource);
 
+export const groceryItemsBus = new Vue();
+const store = new Vuex.Store({
+  state: {
+    count: 0
+  },
+  mutations: {
+    increment(state) {
+      state.count++;
+    }
+  }
+});
 new Vue({
   el: '[data-vue="app"]',
+  store,
   render: h => h(App)
 });
