db093046dbbb6112c2418fbe4538da87a292ec4a
Brian Zelip <bzelip@gmail.com>
2018-10-10 00:31:19 -0400

    update header content

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -3,9 +3,9 @@
     .container.sm-flex.justify-between.mb2
       .sm-flex.flex-center.flex-wrap.mb2.sm-mb0
         h1.m0.lh1
-          a(href="/").fw400 h.site.name
+          a(href="/").fw400 groceries
         h2.mt0.mb0.sm-ml1.h3.fw400.lh1
-          a(href="/").fw400 h.site.byline.toLowerCase()
+          a(href="/").fw400 make a list and send it
       //- block headerAddBtn
       //-   .none.sm-block
       //-     include includes/addItemBtn
