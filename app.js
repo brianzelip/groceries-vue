ca11a01cf126e966731c024278a2f397d6911f2c
Brian Zelip <bzelip@gmail.com>
2018-10-12 12:25:52 -0400

    rm vue-resource and add store with state and getters to app.js

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import VueResource from 'vue-resource';
 import App from './src/App.vue';
 
 Vue.use(Vuex);
-Vue.use(VueResource);
 
 const store = new Vuex.Store({
   state: {
-    count: 0
+    allGroceryItems: []
   },
-  mutations: {
-    increment(state) {
-      state.count++;
+  getters: {
+    allGroceryItemsCount: state => {
+      return state.allGroceryItems.length;
     }
-  }
+  },
+  mutations: {}
 });
 new Vue({
   el: '[data-vue="app"]',
