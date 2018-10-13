bd10914fe45911bc87c9246e58bfea9db6ce8166
Brian Zelip <bzelip@gmail.com>
2018-10-13 14:06:33 -0400

    extract svg edit link into its own component

---
+++ src/components/GroceryForm/GroceryFormItemEditLink.vue
@@ -0,0 +1,18 @@
+<template>
+  <a class="w-16px flex flex-center mx1 sm-mx2 grey" href="#">
+    <editSolid />
+  </a>
+</template>
+
+<script>
+import editSolid from '../icons/edit-solid.vue';
+
+export default {
+  data() {
+    return {};
+  },
+  components: {
+    editSolid
+  }
+};
+</script>
