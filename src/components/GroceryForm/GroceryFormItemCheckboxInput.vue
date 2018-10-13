8257a1f2dc26ac0e7077e48e9f240561a9dff46e
Brian Zelip <bzelip@gmail.com>
2018-10-13 14:06:07 -0400

    break GroceryFormItemCheckboxAndText.vue up into two components

---
+++ src/components/GroceryForm/GroceryFormItemCheckboxInput.vue
@@ -0,0 +1,15 @@
+<template>
+  <input
+    type="checkbox"
+    name="items"
+    :id="slug"
+    :value="slug"
+    class="item__hidden-input">
+</template>
+
+<script>
+export default {
+  props: ['slug']
+};
+</script>
+
---
+++ src/components/GroceryForm/GroceryFormItemCheckboxLabel.vue
@@ -0,0 +1,12 @@
+<template>
+  <label :for="slug" class="item__label col-5">
+    <span class="item__checkbox"></span>
+    <span class="item__text blue">{{ name }}</span>
+  </label>
+</template>
+
+<script>
+export default {
+  props: ['slug', 'name']
+};
+</script>
