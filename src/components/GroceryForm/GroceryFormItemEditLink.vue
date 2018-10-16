056719e3d3c5aa5d1568ba8ef600f46e56ea9ba5
Brian Zelip <bzelip@gmail.com>
2018-10-15 22:02:26 -0400

    saying SCREW IT and just linking to icons in <head> instead of weilding svg in vue and parcel

--- src/components/GroceryForm/GroceryFormItemEditLink.vue
+++ src/components/GroceryForm/GroceryFormItemEditLink.vue
@@ -1,18 +1,5 @@
 <template>
   <a class="w-16px flex flex-center mx1 sm-mx2 grey" href="#">
-    <IconEditSolid></IconEditSolid>
+    <i class="far fa-edit"></i>
   </a>
 </template>
-
-<script>
-import IconEditSolid from '../Icons/IconEditSolid.vue';
-
-export default {
-  data() {
-    return {};
-  },
-  components: {
-    IconEditSolid
-  }
-};
-</script>
