a1e28b993d2d67e4cb24db217fca371283bad0e9
Brian Zelip <bzelip@gmail.com>
2018-10-19 17:24:11 -0400

    abstract out a GroceryFormItem

---
+++ src/components/GroceryForm/GroceryFormItem.vue
@@ -0,0 +1,31 @@
+<template>
+	<li class="item">
+		<GroceryFormItemEditLink></GroceryFormItemEditLink>
+		<GroceryFormItemCheckboxInput :slug="item.slug"></GroceryFormItemCheckboxInput>
+		<GroceryFormItemCheckboxLabel :slug="item.slug" :name="item.name"></GroceryFormItemCheckboxLabel>
+		<GroceryFormItemQtySelector :slug="item.slug"></GroceryFormItemQtySelector>
+		<GroceryFormItemStoresSelector
+			:stores="item.stores"
+			:slug="item.slug"
+			:default-store="item.defaultStore"></GroceryFormItemStoresSelector>
+	</li>
+</template>
+
+<script>
+import GroceryFormItemEditLink from './GroceryFormItemEditLink.vue';
+import GroceryFormItemCheckboxInput from './GroceryFormItemCheckboxInput.vue';
+import GroceryFormItemCheckboxLabel from './GroceryFormItemCheckboxLabel.vue';
+import GroceryFormItemQtySelector from './GroceryFormItemQtySelector.vue';
+import GroceryFormItemStoresSelector from './GroceryFormItemStoresSelector.vue';
+
+export default {
+  components: {
+    GroceryFormItemEditLink,
+    GroceryFormItemCheckboxInput,
+    GroceryFormItemCheckboxLabel,
+    GroceryFormItemQtySelector,
+    GroceryFormItemStoresSelector
+  },
+  props: ['item']
+};
+</script>
