fc824f7544ec5f52adcc1376de66b688043ef0b3
Brian Zelip <bzelip@gmail.com>
2018-10-19 07:43:48 -0400

    re-refactoring GFItemQtySelector around radio buttons to exactly port the UI of groceries into groceries-vue

--- src/components/GroceryForm/GroceryFormItemQtySelector.vue
+++ src/components/GroceryForm/GroceryFormItemQtySelector.vue
@@ -1,14 +1,21 @@
 <template>
   <div class="item__qtySelector col-3">
-    <label :for="`${slug}-qty`" class="mr1 mono purp">qty: </label>
-    <select :name="`${slug}-qty`" :id="`${slug}-qty`" class="p0">
-      <option value="1">1</option>
-      <option value="2">2</option>
-      <option value="3">3</option>
-      <option value="4">4</option>
-      <option value="5">5</option>
-      <option value="6">6</option>
-    </select>
+    <span class="mr1 mono purp">qty:</span>
+    <div
+      v-for="num in [2, 3, 4, 5]"
+      v-bind:key="num"
+      class="item__qtySelector-numWrapper"
+    >
+      <input
+        type="radio"
+        :name="`${slug}-qty`"
+        :id="`${slug}-${num}`"
+        :value="num"
+        class="item__qtySelector-numInput">
+      <label
+        :for="`${slug}-${num}`"
+        class="item__qtySelector-numLabel">{{ num }}</label>
+    </div>
   </div>
 </template>
 
