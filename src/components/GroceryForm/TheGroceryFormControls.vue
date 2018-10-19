110d6bc0c2d2924cb30529dac9bd6c7f75a0ac86
Brian Zelip <bzelip@gmail.com>
2018-10-19 10:23:29 -0400

    rm unused class, and register and render GFAddItemBtn in TheGFControls.vue

--- src/components/GroceryForm/TheGroceryFormControls.vue
+++ src/components/GroceryForm/TheGroceryFormControls.vue
@@ -1,21 +1,21 @@
 <template>
-  <div class="controls flex flex-column sm-flex-row justify-between flex-wrap">
+  <div class="flex flex-column sm-flex-row justify-between flex-wrap">
     <div class="flex flex-center order-2 sm-order-1">
       <input type="submit" value="submit" class="btn btn-primary bg-blue fw400">
       <TheGroceryFormEmailSelector></TheGroceryFormEmailSelector>
     </div>
-    <div class="mb2 sm-mb0 order-1 sm-order-2">
-      <a href="/add" class="btn btn-outline block fw400 center blue">+ add item</a>
-    </div>
+    <GroceryFormAddItemBtn></GroceryFormAddItemBtn>
   </div>
 </template>
 
 <script>
 import TheGroceryFormEmailSelector from './TheGroceryFormEmailSelector.vue';
+import GroceryFormAddItemBtn from './GroceryFormAddItemBtn.vue';
 
 export default {
   components: {
-    TheGroceryFormEmailSelector
+    TheGroceryFormEmailSelector,
+    GroceryFormAddItemBtn
   }
 };
 </script>
