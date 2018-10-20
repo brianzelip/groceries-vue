8fca8d17cbba629ebeb394205cf8277a1280c20f
Brian Zelip <bzelip@gmail.com>
2018-10-20 15:33:43 -0400

    update VueRouter config in app.js

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import Router from 'vue-router';
+import VueRouter from 'vue-router';
+
 import App from './src/App.vue';
+import TheGroceryForm from './src/components/GroceryForm/TheGroceryForm.vue';
+import TheItemForm from './src/components/ItemForm/TheItemForm.vue';
 
 Vue.use(Vuex);
-Vue.use(Router);
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: [
+    { path: '/', component: TheGroceryForm },
+    { path: '/add', component: TheItemForm }
+  ],
+  mode: 'history'
+});
 
 const store = new Vuex.Store({
   state: {
@@ -19,6 +30,7 @@ const store = new Vuex.Store({
 });
 new Vue({
   el: '[data-vue="app"]',
+  router,
   store,
   render: h => h(App)
 });
