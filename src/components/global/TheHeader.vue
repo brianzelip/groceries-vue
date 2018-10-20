2d37535c3108e73fd5286af1127c7de1dcc8fd86
Brian Zelip <bzelip@gmail.com>
2018-10-20 15:38:56 -0400

    rename remaining layout components as global components

--- src/components/global/TheHeader.vue
+++ src/components/global/TheHeader.vue
@@ -3,20 +3,20 @@
     .container.sm-flex.justify-between.mb2
       .sm-flex.flex-center.flex-wrap.mb2.sm-mb0
         h1.m0.lh1
-          a(href="/").fw400 groceries
+          router-link(to="/").fw400 groceries
         h2.mt0.mb0.sm-ml1.h3.fw400.lh1
-          a(href="/").fw400 make a list and send it
+          router-link(to="/").fw400 make a list and send it
       slot(name="headerAddBtn")
         .none.sm-block
-          GroceryFormAddItemBtn
+          AddItemBtn
 </template>
 
 <script>
-import GroceryFormAddItemBtn from '../GroceryForm/GroceryFormAddItemBtn.vue';
+import AddItemBtn from './AddItemBtn.vue';
 
 export default {
   components: {
-    GroceryFormAddItemBtn
+    AddItemBtn
   }
 };
 </script>
