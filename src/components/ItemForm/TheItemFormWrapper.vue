76c3c703fc13ee4f2f9c679455f8af55412600fc
Brian Zelip <bzelip@gmail.com>
2018-10-20 17:31:04 -0400

    create TheItemFormWrapper because we need a h2 and hr

---
+++ src/components/ItemForm/TheItemFormWrapper.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="ItemFormWrapper">
+    <h2 class="fw400">{{ title }}</h2>
+    <hr>
+    <TheItemForm></TheItemForm>
+  </div>
+</template>
+
+<script>
+import TheItemForm from './TheItemForm.vue';
+
+export default {
+  components: {
+    TheItemForm
+  },
+  props: ['title']
+};
+</script>
