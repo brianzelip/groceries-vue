ad02c431f444f42c58c8e09d8867a9f415e2b171
Brian Zelip <bzelip@gmail.com>
2018-10-20 09:54:04 -0400

    import and use vue-router

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import Router from 'vue-router';
 import App from './src/App.vue';
 
 Vue.use(Vuex);
+Vue.use(Router);
 
 const store = new Vuex.Store({
   state: {
