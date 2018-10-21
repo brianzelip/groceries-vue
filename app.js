01eff3a2ac8f2905670766aa6baa324145bb80b7
Brian Zelip <bzelip@gmail.com>
2018-10-21 09:20:47 -0400

    register vue-meta

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import VueRouter from 'vue-router';
 import router from './router.js';
+import Meta from 'vue-meta';
 
 import App from './src/App.vue';
 
 Vue.use(Vuex);
 Vue.use(VueRouter);
+Vue.use(Meta);
 
 const store = new Vuex.Store({
   state: {
