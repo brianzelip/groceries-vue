6eba8b16962923befde4fdb7215d060e81d51d9a
Brian Zelip <bzelip@gmail.com>
2018-11-09 06:33:35 -0500

    refactor storesRef obj into .getters, then get it in StoreSelector.vue

--- src/components/GroceryForm/GroceryFormItemStoresSelector.vue
+++ src/components/GroceryForm/GroceryFormItemStoresSelector.vue
@@ -25,22 +25,17 @@
 </template>
 
 <script>
+import { mapGetters } from 'vuex';
+
 export default {
   data() {
     return {
-      storeChoice: this.defaultStore || null,
-      storesRef: {
-        tj: { name: 'TJ', order: 0 },
-        moms: { name: 'Moms', order: 1 },
-        wineSource: { name: 'Wine Source', order: 2 },
-        howl: { name: 'Howl', order: 3 },
-        farmersMarket: { name: 'Farmers Market', order: 4 },
-        target: { name: 'Target', order: 5 },
-        tjMaxx: { name: 'TJ Maxx', order: 6 },
-        riteAid: { name: 'Rite Aid', order: 7 }
-      }
+      storeChoice: this.defaultStore || null
     };
   },
-  props: ['stores', 'slug', 'defaultStore']
+  props: ['stores', 'slug', 'defaultStore'],
+  computed: {
+    ...mapGetters(['storesRef'])
+  }
 };
 </script>
