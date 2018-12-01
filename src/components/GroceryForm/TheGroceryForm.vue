247ca1b078071526d58c42f1c2d2abfad2ebe8ef
Brian Zelip <bzelip@gmail.com>
2018-12-01 17:12:43 -0500

    remove state rendering in TheGroceryForm, plus vetur formatting

--- src/components/GroceryForm/TheGroceryForm.vue
+++ src/components/GroceryForm/TheGroceryForm.vue
@@ -1,62 +1,58 @@
 <template>
   <form method="POST">
     <ol class="list-reset border rounded">
-			<template v-for="item in allPossibleGroceryItems">
-				<GroceryFormItem
+      <template v-for="item in allPossibleGroceryItems">
+        <GroceryFormItem
           :item="item"
-          :key="item._id">
-          </GroceryFormItem>
-			</template>
+          :key="item._id"
+        ></GroceryFormItem>
+      </template>
     </ol>
     <TheGroceryFormControls v-on:form-submitted="processForm"></TheGroceryFormControls>
-    <hr>
-    <hr>
-    <p>userSelectedItems</p>
-    <pre>{{ USI }}</pre>
   </form>
 </template>
 
 <script>
-import axios from 'axios';
-import { mapGetters, mapActions } from 'vuex';
+  import axios from "axios";
+  import { mapGetters, mapActions } from "vuex";
 
-import GroceryFormItem from './GroceryFormItem.vue';
-import TheGroceryFormControls from './TheGroceryFormControls.vue';
+  import GroceryFormItem from "./GroceryFormItem.vue";
+  import TheGroceryFormControls from "./TheGroceryFormControls.vue";
 
-export default {
-  data() {
-    return {};
-  },
-  components: {
-    GroceryFormItem,
-    TheGroceryFormControls
-  },
-  computed: {
-    ...mapGetters(['allPossibleGroceryItems', 'userSelectedItems']),
-    USI() {
-      return this.$store.getters.userSelectedItems;
-    }
-  },
-  methods: {
-    ...mapActions(['setAllPossibleGroceryItems']),
-    processForm() {
-      console.log('now `processForm()` is in control!');
-      this.$router.push('/submit');
+  export default {
+    data() {
+      return {};
     },
-    postDataForEmail() {
-      axios
-        .post(
-          'https://groceries-vue-api.glitch.me/submit',
-          this.$store.getters.userSelectedItems
-        )
-        .then(console.log('axios.post just worked!'))
-        .catch(error => {
-          console.log('ERROR! ->', error);
-        });
+    components: {
+      GroceryFormItem,
+      TheGroceryFormControls
+    },
+    computed: {
+      ...mapGetters(["allPossibleGroceryItems", "userSelectedItems"]),
+      USI() {
+        return this.$store.getters.userSelectedItems;
+      }
+    },
+    methods: {
+      ...mapActions(["setAllPossibleGroceryItems"]),
+      processForm() {
+        console.log("now `processForm()` is in control!");
+        this.$router.push("/submit");
+      },
+      postDataForEmail() {
+        axios
+          .post(
+            "https://groceries-vue-api.glitch.me/submit",
+            this.$store.getters.userSelectedItems
+          )
+          .then(console.log("axios.post just worked!"))
+          .catch(error => {
+            console.log("ERROR! ->", error);
+          });
+      }
+    },
+    created() {
+      this.setAllPossibleGroceryItems();
     }
-  },
-  created() {
-    this.setAllPossibleGroceryItems();
-  }
-};
+  };
 </script>
