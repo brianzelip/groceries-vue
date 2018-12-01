daef6602dd0eba9173d79704ebc0a9671ae5d7a3
Brian Zelip <bzelip@gmail.com>
2018-12-01 06:20:03 -0500

    remove code!

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -14,10 +14,6 @@ export const UPDATE_ITEM_IN_USER_SELECTED_ITEMS = (state, payload) => {
   Vue.set(state.userSelectedItems[payload._id], payload.key, payload.value);
 };
 
-export const UPDATE_ITEM_QTY = (state, payload) => {
-  state.userSelectedItems[payload._id].qty = payload.qty;
-};
-
 export const UPDATE_ITEM_STORE = (state, payload) => {
   state.userSelectedItems[payload._id].store = payload.store;
   payload.storeArea
