b5a819140fb2587878159b9025059737881de6f0
Brian Zelip <bzelip@gmail.com>
2018-12-01 08:49:20 -0500

    refactor GFICheckboxLabel.vue to dispatch state actions

--- src/components/GroceryForm/GroceryFormItemCheckboxLabel.vue
+++ src/components/GroceryForm/GroceryFormItemCheckboxLabel.vue
@@ -1,15 +1,43 @@
 <template>
   <label
-    :for="slug"
+    :for="item.slug"
     class="item__label col-5"
-    v-on:click="$emit('item-selection-change')">
+    @click="isSelected = !isSelected"
+    v-on:click="$emit('item-selection-change')"
+  >
     <span class="item__checkbox"></span>
-    <span class="item__text blue">{{ name }}</span>
+    <span class="item__text blue">{{ item.name }}</span>
   </label>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+
 export default {
-  props: ['slug', 'name']
+  data() {
+    return {
+      isSelected: false
+    };
+  },
+  props: ["item"],
+  methods: {
+    ...mapActions([
+      "addItemToUserSelectedItems",
+      "removeItemInUserSelectedItems"
+    ])
+  },
+  watch: {
+    isSelected() {
+      this.isSelected
+        ? this.addItemToUserSelectedItems({
+            _id: this.item._id,
+            name: this.item.name,
+            store: this.item.defaultStore || null,
+            tjArea: this.item.tjArea,
+            momsArea: this.item.momsArea
+          })
+        : this.removeItemInUserSelectedItems(this.item._id);
+    }
+  }
 };
 </script>
