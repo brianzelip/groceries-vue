7fa8f1e19ff44012b090c926afa9b37ef6cfc9df
Brian Zelip <bzelip@gmail.com>
2019-02-06 06:21:00 -0500

    state.js: Delete emailTemplate, mv it to getters

--- src/store/state.js
+++ src/store/state.js
@@ -13,30 +13,3 @@ export const storesRef = {
   riteAid: { name: 'Rite Aid', order: 7 },
   noStore: { name: null, order: 99 }
 };
-
-export const emailTemplate = (store, items) => {
-  return `
-    <h1 style="font-size: 1.25rem; font-weight: 400;">${
-      store !== 'noStore' ? storesRef[store].name.toLowerCase() : 'other'
-    }</h1>
-    <ol style="margin-bottom: 2rem; padding-left: 0; list-style: none;">
-      ${items
-        .map(
-          item =>
-            `<li>
-          <input
-            type="checkbox"
-            value="${item.name}"
-            id="${item.name}"
-            name="item"
-          >
-          <label for="${item.name}">
-            ${item.name}${item.qty ? ` (x${item.qty})` : ''}
-          </label>
-        </li>          
-      `
-        )
-        .join('')}
-    </ol>
-  `;
-};
