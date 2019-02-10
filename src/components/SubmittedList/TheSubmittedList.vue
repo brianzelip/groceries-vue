1a647b8468af60182051120577880da8a06e7c92
Brian Zelip <bzelip@gmail.com>
2019-02-10 11:51:20 -0500

    SubmittedList: Remove debuggers

--- src/components/SubmittedList/TheSubmittedList.vue
+++ src/components/SubmittedList/TheSubmittedList.vue
@@ -4,25 +4,15 @@
     <h3 class="fw400">here's what the email sent to XXX looks like:</h3>
     <hr>
     <section v-html="emailBody"></section>
-    <button
-      @click="resetUSI"
-      class="btn btn-primary"
-    >reset userSelectedItems</button>
   </div>
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
+import { mapGetters } from "vuex";
 
 export default {
   computed: {
     ...mapGetters(["emailBody"])
-  },
-  methods: {
-    ...mapActions(["resetUserSelectedItems"]),
-    resetUSI() {
-      this.resetUserSelectedItems();
-    }
   }
 };
 </script>
