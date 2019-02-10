ca6e44736392673c659fb63602f92af80b79c1f8
Brian Zelip <bzelip@gmail.com>
2019-02-10 11:48:33 -0500

    getters: Rewrite storeTemplate html with readable whitespace - it renders in email just fine

--- src/store/getters.js
+++ src/store/getters.js
@@ -91,27 +91,25 @@ export const emailBody = (state, getters) => {
   }, ``);
 
   function storeTemplate(store, items) {
-    return `<h1 style="font-size: 1.25rem; font-weight: 400;">${
+    return `
+    <h1 style="font-size: 1.25rem; font-weight: 400;">${
       store !== 'noStore' ? storesRef[store].name.toLowerCase() : 'other'
     }</h1>
-    <ol style="margin-bottom: 2rem; padding-left: 0; list-style: none;">
-      ${items
-        .map(
-          item => `
-      <li>
-        <input
-          type="checkbox"
-          value="${item.name}"
-          id="${item.name}"
-          name="item"
-        >
-        <label for="${item.name}">
-          ${item.name}${item.qty ? ` (x${item.qty})` : ''}
-        </label>
-      </li>          
-        `
-        )
-        .join('')}
+    <ol style="margin-bottom: 2rem; padding-left: 0; list-style: none;">${items
+      .map(
+        item =>
+          `<li>
+            <input
+              type="checkbox"
+              value="${item.name}"
+              id="${item.name}"
+              name="item">
+            <label for="${item.name}">${item.name}${
+            item.qty ? ` (x${item.qty})` : ''
+          }</label>
+          </li>`
+      )
+      .join('')}
     </ol>`;
   }
 
