b3c74daaa26a2e408af8397b272ce9170dc77eec
Brian Zelip <bzelip@gmail.com>
2019-02-20 09:52:13 -0500

    NewForm: add new item form component to test

---
+++ src/components/ItemForm/NewForm.vue
@@ -0,0 +1,22 @@
+<template>
+  <form>
+    <label
+      class="block"
+      for="name"
+    >name
+      <span class="purp">*</span>
+    </label>
+    label(for="name").block name
+    span.purp *
+  </form>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["storesRef"])
+  }
+};
+</script>
