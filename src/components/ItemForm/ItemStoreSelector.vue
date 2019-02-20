02680d87018b4261706721d07924697da24675e7
Brian Zelip <bzelip@gmail.com>
2019-02-20 17:03:15 -0500

    ItemStoreSelector.vue: Create component, which refactors two sections from groceries into one component here via props

---
+++ src/components/ItemForm/ItemStoreSelector.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="mt2">
+    <h1 class="m0 h4 regular">{{ header[inputType] }}:</h1>
+    <div class="flex flex-column sm-flex-row sm-flex-wrap border-bottom">
+      <div
+        :key="i"
+        v-for="(store, i) in Object.keys(storesRef).filter(store => store != 'noStore')"
+      >
+        <input
+          :id="`${inputType}-${store}`"
+          :name="`${name[inputType]}`"
+          :type="`${inputType}`"
+          :value="`${store}`"
+        >
+        <label
+          :for="`${inputType}-${store}`"
+          class="mr2"
+        >{{ storesRef[store].name }}</label>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  data() {
+    return {
+      header: {
+        checkbox: "stores where we get this item",
+        radio: "our default store of choice"
+      },
+      name: {
+        checkbox: "stores",
+        radio: "defaultStore"
+      }
+    };
+  },
+  props: ["inputType"],
+  computed: {
+    ...mapState(["storesRef"])
+  }
+};
+</script>
