13ac8ee3ffd5b9dbd27d67cd64906c0e705629f9
Brian Zelip <bzelip@gmail.com>
2019-02-20 10:53:03 -0500

    TheItemNameInput.vue: Create component, pass item name input to parent

---
+++ src/components/ItemForm/TheItemNameInput.vue
@@ -0,0 +1,35 @@
+<template>
+  <section>
+    <label
+      class="block"
+      for="name"
+    >
+      <h1 class="mt1 mb1 h4 regular">
+        name
+        <span class="purp">*</span>
+      </h1>
+    </label>
+    <input
+      class="col-12 sm-col-8 md-col-6 field"
+      id="name"
+      name="name"
+      type="text"
+      v-model="name"
+    >
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: ""
+    };
+  },
+  watch: {
+    name() {
+      this.$emit("input", this.name);
+    }
+  }
+};
+</script>
