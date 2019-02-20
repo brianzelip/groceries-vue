88356e9f9c36e51578df804b4b94f42949023c0d
Brian Zelip <bzelip@gmail.com>
2019-02-20 17:10:42 -0500

    NewForm.vue: Rename TheItemForm.vue

--- src/components/ItemForm/TheItemForm.vue
+++ src/components/ItemForm/TheItemForm.vue
@@ -1,34 +1,34 @@
-<template lang="pug">
-  form(action="/add" method="POST")
-    label(for="name").block name
-      span.purp *
-    input(type="text" name="name" id="name").col-12.sm-col-8.md-col-6.field
-    .mt2 stores where we get this item:
-      .flex.flex-column.sm-flex-row.sm-flex-wrap.border-bottom
-        div(v-for="store in Object.keys(storesRef)")
-          input(
-            type="checkbox"
-            name="stores"
-            :id="`checkbox-${store}`"
-            :value="`${store}`")
-          label(:for="`checkbox-${store}`").mr2 {{ storesRef[store].name }}
-    .mt2 our default store of choice:
-      .flex.flex-column.sm-flex-row.sm-flex-wrap.border-bottom
-        div(v-for="store in Object.keys(storesRef)")
-          input(
-            type="radio"
-            name="defaultStore"
-            :id="`radio-${store}`"
-            :value="`${store}`")
-          label(for="`radio-${store}`").mr2 {{ storesRef[store].name }}
+<template>
+  <form>
+    <TheItemNameInput v-on:input="updateName"></TheItemNameInput>
+    <ItemStoreSelector inputType="checkbox"></ItemStoreSelector>
+    <ItemStoreSelector inputType="radio"></ItemStoreSelector>
+  </form>
 </template>
 
 <script>
 import { mapState } from "vuex";
 
+import TheItemNameInput from "./TheItemNameInput.vue";
+import ItemStoreSelector from "./ItemStoreSelector.vue";
+
 export default {
+  data() {
+    return {
+      name: ""
+    };
+  },
   computed: {
     ...mapState(["storesRef"])
+  },
+  methods: {
+    updateName(name) {
+      this.name = name;
+    }
+  },
+  components: {
+    TheItemNameInput,
+    ItemStoreSelector
   }
 };
 </script>
