05b2377655a6bb90e03de73f46b18f93669a5af0
Brian Zelip <bzelip@gmail.com>
2019-02-20 14:51:01 -0500

    ItemFormContainer: Render NewForm for debugging

--- src/components/ItemForm/TheItemFormContainer.vue
+++ src/components/ItemForm/TheItemFormContainer.vue
@@ -3,15 +3,18 @@
     <h2 class="fw400">{{ heading }}</h2>
     <hr>
     <TheItemForm></TheItemForm>
+    <NewForm></NewForm>
   </div>
 </template>
 
 <script>
 import TheItemForm from "./TheItemForm.vue";
+import NewForm from "./NewForm.vue";
 
 export default {
   components: {
-    TheItemForm
+    TheItemForm,
+    NewForm
   },
   props: ["heading"]
 };
