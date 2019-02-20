5de554840b90ca9a1d3cbd5a579af9a73c6224d4
Brian Zelip <bzelip@gmail.com>
2019-02-20 17:11:21 -0500

    TheItemFormContainer: Unregister NewForm child

--- src/components/ItemForm/TheItemFormContainer.vue
+++ src/components/ItemForm/TheItemFormContainer.vue
@@ -3,18 +3,15 @@
     <h2 class="fw400">{{ heading }}</h2>
     <hr>
     <TheItemForm></TheItemForm>
-    <NewForm></NewForm>
   </div>
 </template>
 
 <script>
 import TheItemForm from "./TheItemForm.vue";
-import NewForm from "./NewForm.vue";
 
 export default {
   components: {
-    TheItemForm,
-    NewForm
+    TheItemForm
   },
   props: ["heading"]
 };
