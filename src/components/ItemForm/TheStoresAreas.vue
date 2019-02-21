b269d193eab99ddd75dec4d6abdf91cd10d53894
Brian Zelip <bzelip@gmail.com>
2019-02-21 08:29:58 -0500

    StoresAreas.vue: Flesh out markup a bit

--- src/components/ItemForm/TheStoresAreas.vue
+++ src/components/ItemForm/TheStoresAreas.vue
@@ -6,27 +6,23 @@
           :for="`${store}-area`"
           :key="`label-${store}-${i}`"
           class="block"
-        >{{store}}area</label>
+        >{{ storesRef[store].name }} area</label>
         <input
           :id="`${store}-area`"
           :key="`input-${store}-${i}`"
           :name="`${store}Area`"
           class="block field w-90px mb2"
+          disabled
           type="number"
         >
-        <!-- label(for=`${store}-area`).block #{storesWithAreas[store]} area -->
-        <!-- input(type="number" id=`${store}-area` name=`${store}Area` disabled=(h.itemHasStore(item, store) ? false : true) value=(item[`${store}Area`])).block.field.w-90px.mb2 -->
       </template>
-      <!-- each store in Object.keys(storesWithAreas)
-        label(for=`${store}-area`).block #{storesWithAreas[store]} area
-        input(type="number" id=`${store}-area` name=`${store}Area` disabled=(h.itemHasStore(item, store) ? false : true) value=(item[`${store}Area`])).block.field.w-90px.mb2
-    .flex.flex-auto.ml2
-      each store in h.storesAreas
-        - let caption = store[0];
-        - let storeName = store[0].split(' ')[0].toLowerCase();
-          div(id=`${storeName}-areas-ref` hidden=((h.itemHasStore(item, storeName) ? false : true))).col-6.px2
-            .overflow-scroll
-              table(class="table-light overflow-hidden border rounded").mono
+      <div class="flex flex-auto ml2">
+        <div
+          :id="`${storeName}-areas-ref`"
+          class="col-6 px2"
+        >
+          <div class="overflow-scroll">
+            <!-- table(class="table-light overflow-hidden border rounded").mono
                 caption= caption
                 thead.bg-light-grey
                   tr
@@ -36,7 +32,10 @@
                   each key in Object.keys(store[1]).sort()
                     tr
                       td= key
-      td= store[1][key]-->
+            td= store[1][key]-->
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 </template>
