042e9b3066728f42f6317c94313c753bceaa3905
Brian Zelip <bzelip@gmail.com>
2019-02-21 14:27:54 -0500

    state: Refactor store Areas reference objs

--- src/store/state.js
+++ src/store/state.js
@@ -16,20 +16,21 @@ export const storesRef = {
 
 export const storesWithAreas = ['tj', 'moms'];
 
-export const tjAreas = {
-  '1': 'raw nuts, fruit, vegetables',
-  '2': 'hummus, eggs, soy milk, oj, cream cheese, sour cream',
-  '3':
-    'frozen pizza, frozen stuff, cookies and chocolate covered stuff, frozen fish, etc',
-  '4': 'crackers, cereal, tortilla chips, cheetos',
-  '5': 'whole milk, cream, butter',
-  '6':
-    'dog biscuits, broth, pasta, pasta sauce, sponges, tooth paste, tissue, syrup, tahini, soy, olive oil, etc',
-  '7': 'bread, raw meat, cheese, water'
-};
-
-export const momsAreas = {
-  '1': 'vegetables, fruit, raw meat',
-  '2': 'yougurt, milk, cream, cheese, pickles, bulk granola, bulk beans',
-  '3': 'bread, dried fruit'
+export const storesAreasRef = {
+  tjAreas: {
+    '1': 'raw nuts, fruit, vegetables',
+    '2': 'hummus, eggs, soy milk, oj, cream cheese, sour cream',
+    '3':
+      'frozen pizza, frozen stuff, cookies and chocolate covered stuff, frozen fish, etc',
+    '4': 'crackers, cereal, tortilla chips, cheetos',
+    '5': 'whole milk, cream, butter',
+    '6':
+      'dog biscuits, broth, pasta, pasta sauce, sponges, tooth paste, tissue, syrup, tahini, soy, olive oil, etc',
+    '7': 'bread, raw meat, cheese, water'
+  },
+  momsAreas: {
+    '1': 'vegetables, fruit, raw meat',
+    '2': 'yougurt, milk, cream, cheese, pickles, bulk granola, bulk beans',
+    '3': 'bread, dried fruit'
+  }
 };
