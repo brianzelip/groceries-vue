c49be293015be906cb0a41d0c6f393f5ada0956c
Brian Zelip <bzelip@gmail.com>
2019-02-26 15:16:58 -0500

    ItemStoreAreaLabel.vue: Create child component

---
+++ src/components/ItemForm/ItemStoreAreaLabel.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex flex-column">
+    <label
+      :for="`${store}-area`"
+      :key="`label-${store}-${i}`"
+      class="block"
+    >{{ storesRef[store].name }} area</label>
+  </div>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  props: ["store"],
+  computed: {
+    ...mapState(["storesRef"])
+  }
+};
+</script>
+
