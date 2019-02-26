a4ae2167333cfe1775d7b3d9e7c3d8ea65bfaa43
Brian Zelip <bzelip@gmail.com>
2019-02-26 15:16:40 -0500

    ItemStoreAreaInput.vue: Create child component

---
+++ src/components/ItemForm/ItemStoreAreaInput.vue
@@ -0,0 +1,17 @@
+<template>
+  <input
+    :id="`${store}-area`"
+    :key="`input-${store}-${i}`"
+    :name="`${store}Area`"
+    class="block field w-90px mb2"
+    disabled
+    type="number"
+  >
+</template>
+
+<script>
+export default {
+  props: ["store"]
+};
+</script>
+
