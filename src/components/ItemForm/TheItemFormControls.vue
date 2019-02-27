2d16ddef0eee279fe99e438f3b9abc7e27c6f47d
Brian Zelip <bzelip@gmail.com>
2019-02-27 17:06:09 -0500

    TheItemFormControls.vue: Create component, TODO: post state.newItem to api

---
+++ src/components/ItemForm/TheItemFormControls.vue
@@ -0,0 +1,31 @@
+<template>
+  <input
+    @click.prevent="postData"
+    class="mt2 btn btn-primary bg-blue fw400"
+    type="submit"
+    value="Save →"
+  >
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  methods: {
+    post() {
+      // TODO
+      // submit state.newItem to back end api
+      // ie (via TheGroceryForm.vue):
+      // axios
+      //   .post("https://groceries-vue-api.glitch.me/submit", {
+      //     html: this.emailBody
+      //   })
+      //   .then(console.log("axios.post just posted HELLO WORLD!"))
+      //   .then(this.$router.push("/submit"))
+      //   .catch(error => {
+      //     console.log("ERROR! ->", error);
+      //   });
+    }
+  }
+};
+</script>
