730b002dfb309002d3b20db8eca96b337e4b6c32
Brian Zelip <bzelip@gmail.com>
2019-02-27 16:27:47 -0500

    ItemStoreAreaInput: Wire up state for newItem.tjArea and newItem.momsArea

--- src/components/ItemForm/ItemStoreAreaInput.vue
+++ src/components/ItemForm/ItemStoreAreaInput.vue
@@ -4,14 +4,33 @@
     :key="`input-${store}`"
     :name="`${store}Area`"
     class="block field w-90px mb2"
-    disabled
     type="number"
+    v-model="input"
   >
 </template>
 
 <script>
+import { mapState, mapActions } from "vuex";
+
 export default {
-  props: ["store"]
+  props: ["store"],
+  computed: {
+    ...mapState(["newItem"]),
+    input: {
+      get() {
+        return this.newItem[`${this.capFirstLetter(this.store)}Area`];
+      },
+      set(payload) {
+        this[`setNewItem${this.capFirstLetter(this.store)}Area`](payload);
+      }
+    }
+  },
+  methods: {
+    ...mapActions(["setNewItemTjArea", "setNewItemMomsArea"]),
+    capFirstLetter(string) {
+      return `${string.slice(0, 1).toUpperCase()}${string.slice(1)}`;
+    }
+  }
 };
 </script>
 
