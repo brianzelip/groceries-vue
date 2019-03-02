a790349baba163c70ec1a9fd671d8bafb6186b2d
Brian Zelip <bzelip@gmail.com>
2019-03-02 06:04:47 -0500

    TheItemStoresAreas: Register Refs component, remove unused data prop

--- src/components/ItemForm/TheItemStoresAreas.vue
+++ src/components/ItemForm/TheItemStoresAreas.vue
@@ -4,41 +4,7 @@
       :momsIsSelected="momsIsSelected"
       :tjIsSelected="tjIsSelected"
     ></TheItemStoresAreasInputs>
-    <div class="flex flex-auto ml2">
-      <div
-        :id="`${store}-areas-ref`"
-        :key="`${i}-${store}-areas-ref`"
-        class="col-6 px2"
-        v-for="(store, i) in storesWithAreas"
-      >
-        <div class="overflow-scroll">
-          <table class="table-light overflow-hidden border rounded mono">
-            <caption>{{`${storesRef[store].name} areas`}}</caption>
-            <thead class="bg-light-grey">
-              <tr>
-                <th
-                  class="fw400"
-                  scope="col"
-                >area</th>
-                <th
-                  class="fw400"
-                  scope="col"
-                >items</th>
-              </tr>
-            </thead>
-            <tbody class="mono">
-              <tr
-                :key="i"
-                v-for="(key, i) in Object.keys(storesAreasRef[`${store}Areas`])"
-              >
-                <td>{{ key }}</td>
-                <td>{{ storesAreasRef[`${store}Areas`][key] }}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </div>
-    </div>
+    <TheItemStoresAreasRefs></TheItemStoresAreasRefs>
   </section>
 </template>
 
@@ -46,14 +12,13 @@
 import { mapState } from "vuex";
 
 import TheItemStoresAreasInputs from "./TheItemStoresAreasInputs.vue";
+import TheItemStoresAreasRefs from "./TheItemStoresAreasRefs.vue";
 
 export default {
-  data() {
-    return {};
-  },
   props: ["tjIsSelected", "momsIsSelected"],
   components: {
-    TheItemStoresAreasInputs
+    TheItemStoresAreasInputs,
+    TheItemStoresAreasRefs
   },
   computed: {
     ...mapState(["storesRef", "storesWithAreas", "storesAreasRef"])
