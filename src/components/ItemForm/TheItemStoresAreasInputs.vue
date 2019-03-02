3ceed3ecbd10ad0b3cfecfa7e4a958692e2f860f
Brian Zelip <bzelip@gmail.com>
2019-03-02 05:58:10 -0500

    TheItemStoresAreasInputs.vue: Create component abstracted from TheItemStoresAreas.vue

---
+++ src/components/ItemForm/TheItemStoresAreasInputs.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="flex flex-column">
+    <div v-if="tjIsSelected">
+      <ItemStoreAreaLabel store="tj"></ItemStoreAreaLabel>
+      <ItemStoreAreaInput store="tj"></ItemStoreAreaInput>
+    </div>
+    <div v-if="momsIsSelected">
+      <ItemStoreAreaLabel store="moms"></ItemStoreAreaLabel>
+      <ItemStoreAreaInput store="moms"></ItemStoreAreaInput>
+    </div>
+  </div>
+</template>
+
+<script>
+import ItemStoreAreaLabel from "./ItemStoreAreaLabel.vue";
+import ItemStoreAreaInput from "./ItemStoreAreaInput.vue";
+
+export default {
+  props: ["tjIsSelected", "momsIsSelected"],
+  components: {
+    ItemStoreAreaLabel,
+    ItemStoreAreaInput
+  }
+};
+</script>
