48521630eafd051b2385b886f4c7a308a614bdba
Brian Zelip <bzelip@gmail.com>
2019-03-03 14:51:44 -0500

    TheGroceryFormControls: Refactor submit from custom event to vuex

--- src/components/GroceryForm/TheGroceryFormControls.vue
+++ src/components/GroceryForm/TheGroceryFormControls.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-column sm-flex-row justify-between flex-wrap">
     <div class="flex flex-center order-2 sm-order-1">
       <input
-        @click.prevent="$emit('form-submitted')"
+        @click.prevent="submitForm"
         class="btn btn-primary bg-blue fw400"
         type="submit"
         value="submit"
@@ -14,6 +14,9 @@
 </template>
 
 <script>
+import axios from "axios";
+import { mapGetters } from "vuex";
+
 import TheGroceryFormEmailSelector from "./TheGroceryFormEmailSelector.vue";
 import GroceryFormAddItemBtn from "../global/AddItemBtn.vue";
 
@@ -21,6 +24,22 @@ export default {
   components: {
     TheGroceryFormEmailSelector,
     GroceryFormAddItemBtn
+  },
+  computed: {
+    ...mapGetters(["emailBody"])
+  },
+  methods: {
+    submitForm() {
+      axios
+        .post("https://groceries-vue-api.glitch.me/submit", {
+          html: this.emailBody
+        })
+        .then(console.log("axios.post just posted HELLO WORLD!"))
+        .then(this.$router.push("/submit"))
+        .catch(error => {
+          console.log("ERROR! ->", error);
+        });
+    }
   }
 };
 </script>
