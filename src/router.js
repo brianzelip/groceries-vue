a465073fd7a8db49896b1aa1593e0423807eeb06
Brian Zelip <bzelip@gmail.com>
2019-03-04 10:51:50 -0500

    router: Swap out TheItemFormContainer for TheItemForm

--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import VueRouter from 'vue-router';
 
 import TheGroceryForm from './components/GroceryForm/TheGroceryForm.vue';
-import TheItemFormContainer from './components/ItemForm/TheItemFormContainer.vue';
+import TheItemForm from './components/ItemForm/TheItemForm.vue';
 import TheSubmittedList from './components/SubmittedList/TheSubmittedList.vue';
 
 const routes = [
@@ -13,13 +13,13 @@ const routes = [
   {
     path: '/add',
     name: 'add',
-    component: TheItemFormContainer,
+    component: TheItemForm,
     props: { routeName: 'add' }
   },
   {
     path: '/edit/:_id',
     name: 'edit',
-    component: TheItemFormContainer,
+    component: TheItemForm,
     props: { routeName: 'edit' }
   },
   {
