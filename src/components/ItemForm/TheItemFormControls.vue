e201965b67a8442c427acabc67250b18bdcaea95
Brian Zelip <bzelip@gmail.com>
2019-03-04 20:54:20 -0500

    TheItemFormControls: Create update method, and higher order method, register state

--- src/components/ItemForm/TheItemFormControls.vue
+++ src/components/ItemForm/TheItemFormControls.vue
@@ -1,6 +1,6 @@
 <template>
   <input
-    @click.prevent="create"
+    @click.prevent="postData"
     class="mt2 btn btn-primary bg-blue fw400"
     type="submit"
     value="Save →"
@@ -8,25 +8,48 @@
 </template>
 
 <script>
-// if edit item, save should do something else
-
 import axios from "axios";
 import { mapState } from "vuex";
 
 export default {
   computed: {
-    ...mapState(["itemFormItem"])
+    ...mapState(["api", "itemFormItem"])
   },
   methods: {
+    postData() {
+      this.$route.name === "add" ? this.create() : this.update();
+    },
     create() {
       axios
-        .post("https://groceries-vue-api.glitch.me/create", this.itemFormItem)
+        .post(`${this.api}/create`, this.itemFormItem)
         .then(this.$router.push("/"))
         .catch(error => {
           console.log("ERROR!:::", error);
         });
     },
-    update() {}
+    update() {
+      if (this.$route.params._id != this.itemFormItem._id) {
+        console.log(
+          `RATS!\n\n${this.$route.params._id} != ${this.itemFormItem._id} :(`
+        );
+        return;
+      }
+      axios
+        .post(`${this.api}/edit/${this.$route.params._id}`, this.itemFormItem)
+        .then(
+          this.$router.push({
+            name: "home",
+            params: {
+              flash: `Successfully updated <strong>${
+                this.itemFormItem.name
+              }</strong>!`
+            }
+          })
+        )
+        .catch(error => {
+          console.log("ERROR!:::", error);
+        });
+    }
   }
 };
 </script>
