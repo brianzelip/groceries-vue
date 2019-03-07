46d7fcb05ceb5e4aca8917b67f1daf9820f9539d
Brian Zelip <bzelip@gmail.com>
2019-03-07 14:30:35 -0500

    TheSubmittedList: Add computed prop and render it in markup

--- src/components/SubmittedList/TheSubmittedList.vue
+++ src/components/SubmittedList/TheSubmittedList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h2 class="fw400">it worked 🎉</h2>
-    <h3 class="fw400">here's what the email sent to XXX looks like:</h3>
+    <h3 class="fw400">here's what the email sent to {{ recipients }} looks like:</h3>
     <hr>
     <section v-html="emailBody"></section>
   </div>
@@ -13,7 +13,12 @@ import { mapState, mapGetters } from "vuex";
 export default {
   computed: {
     ...mapState(["emailTo"]),
-    ...mapGetters(["emailBody"])
+    ...mapGetters(["emailBody"]),
+    recipients() {
+      return this.emailTo.length === 1
+        ? this.emailTo[0]
+        : this.emailTo.join(" and ");
+    }
   }
 };
 </script>
