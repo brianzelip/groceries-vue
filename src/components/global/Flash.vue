bd17ac400b4db8b2448c6d296d840cbee42d1907
Brian Zelip <bzelip@gmail.com>
2019-03-07 16:33:15 -0500

    Flash.vue: Add pug markup from groceries

--- src/components/global/Flash.vue
+++ src/components/global/Flash.vue
@@ -1,5 +1,15 @@
 <template>
   <section>
     <h1>⚡</h1>
+    <!--if locals.flashes && Object.keys(locals.flashes).length > 0
+        section
+          .container
+            //- pre= h.dump(locals.flashes)
+            - const categories = Object.keys(locals.flashes)
+            each category in categories
+              each message in flashes[category]
+                section(class=`flex flex-center flash flash--${category} mb3`)
+                  h1.m0.h4.regular.flex-grow!= message
+                  a.btn.h3.p0.cursor(onClick="this.parentElement.remove()") &times;-->
   </section>
 </template>
