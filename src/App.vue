fe4655712404da53d6673113d09ea45a9737713e
Brian Zelip <bzelip@gmail.com>
2019-03-07 23:25:35 -0500

    App.vue: Register getter and Flash component

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
   div(data-vue="app")
     TheHeader
+    Flash(v-if="haveAtLeastOneFlash")
     main
       .container
         router-view
@@ -8,14 +9,18 @@
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 import TheHeader from "./components/global/TheHeader.vue";
 import TheFooter from "./components/global/TheFooter.vue";
+import Flash from "./components/Flash/Flash.vue";
 
 export default {
   data() {
     return {};
   },
   computed: {
+    ...mapGetters(["haveAtLeastOneFlash"]),
     title() {
       const appTitle = "groceries-vue";
       return this.$route.name === "home"
@@ -25,7 +30,8 @@ export default {
   },
   components: {
     TheHeader,
-    TheFooter
+    TheFooter,
+    Flash
   },
   metaInfo() {
     return {
