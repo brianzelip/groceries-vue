3200f8aa6a0c86720662f508197bf71232bb15f1
Brian Zelip <bzelip@gmail.com>
2019-03-11 16:25:55 -0400

    AddItemBtn: Reset flashes state on click if flashes

--- src/components/global/AddItemBtn.vue
+++ src/components/global/AddItemBtn.vue
@@ -1,8 +1,27 @@
 <template>
   <div class="mb2 sm-mb0 order-1 sm-order-2">
     <router-link
+      @click.native="resetState"
       class="btn btn-outline block fw400 center blue"
       to="/add"
     >+ add item</router-link>
   </div>
 </template>
+
+<script>
+import { mapState, mapActions } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["flashes"])
+  },
+  methods: {
+    ...mapActions(["resetFlashes"]),
+    resetState() {
+      if (this.flashes.length > 0) {
+        this.resetFlashes();
+      }
+    }
+  }
+};
+</script>
