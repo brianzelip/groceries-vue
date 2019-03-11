cb91dab6d5ab46a95b38de6794135a3fef6c3cb6
Brian Zelip <bzelip@gmail.com>
2019-03-11 12:50:05 -0400

    Flash/ -> Flashes/: Rename Flashes dir, make higer order TheFlashes component

---
+++ src/components/Flashes/Flash.vue
@@ -0,0 +1,34 @@
+<template>
+  <div
+    :class="[`flash--${flash.flashType}`]"
+    class="container flex flex-center mb3 flash"
+  >
+    <span class="flex-grow">
+      <h1
+        class="inline-block m0 h4 regular"
+        v-html="success(flash.formType, flash.name)"
+      ></h1>
+      <FlashViewItemLink :_id="flash._id"></FlashViewItemLink>
+    </span>
+    <button
+      class="btn h3 p0 cursor"
+      href
+    >&times;</button>
+  </div>
+</template>
+
+<script>
+import FlashViewItemLink from "./FlashViewItemLink.vue";
+
+export default {
+  props: ["flash"],
+  components: {
+    FlashViewItemLink
+  },
+  methods: {
+    success(formType, name) {
+      return `Successfully ${formType}ed <strong>${name}</strong>!`;
+    }
+  }
+};
+</script>
---
+++ src/components/Flashes/TheFlashes.vue
@@ -0,0 +1,26 @@
+<template>
+  <section>
+    <div
+      :key="i"
+      v-for="(flash, i) in flashes"
+    >
+      <Flash :flash="flash"></Flash>
+    </div>
+  </section>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+import Flash from "./Flash.vue";
+
+export default {
+  components: {
+    Flash
+  },
+  computed: {
+    ...mapState(["flashes"])
+  }
+};
+</script>
+
