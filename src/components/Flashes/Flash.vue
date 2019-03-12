7bcb0dbf0fe30d2cd531cbd3c270a8d7009ecee9
Brian Zelip <bzelip@gmail.com>
2019-03-12 06:20:50 -0400

    Flash.vue: Refactor new method to handle all flash msgs

--- src/components/Flashes/Flash.vue
+++ src/components/Flashes/Flash.vue
@@ -6,9 +6,12 @@
     <span class="flex-grow">
       <h1
         class="inline-block m0 h4 regular"
-        v-html="success(flash.formType, flash.name)"
+        v-html="flashMsg()"
       ></h1>
-      <FlashViewItemLink :_id="flash._id"></FlashViewItemLink>
+      <FlashViewItemLink
+        :_id="flash._id"
+        v-if="showFVIL"
+      ></FlashViewItemLink>
     </span>
     <button
       @click="close"
@@ -26,10 +29,31 @@ export default {
   components: {
     FlashViewItemLink
   },
+  computed: {
+    showFVIL() {
+      return Object.keys(this.flash).includes("_id");
+    }
+  },
   methods: {
     ...mapActions(["removeFlash"]),
-    success(formType, name) {
-      return `Successfully ${formType}ed <strong>${name}</strong>!`;
+    flashMsg() {
+      if (this.flash.flashType === "success") {
+        return `Successfully ${this.flash.formType}ed <strong>${
+          this.flash.name
+        }</strong>!`;
+      }
+      if (
+        this.flash.flashType === "error" &&
+        this.flash.formType === "grocery"
+      ) {
+        return this.flash.error === "items"
+          ? `Add an item to the list will ya!`
+          : `Select an email recipient will ya!`;
+      }
+
+      if (this.flash.flashType === "error" && this.flash.formType === "item") {
+        return `Name the item will ya!`;
+      }
     },
     close() {
       this.removeFlash(this.flash._id);
