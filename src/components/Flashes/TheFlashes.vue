496ce5feac7b95bbf773ed3b2c86192193280317
Brian Zelip <bzelip@gmail.com>
2019-03-13 11:06:31 -0400

    TheFlashes: Move <transition> into Flash chid comp

--- src/components/Flashes/TheFlashes.vue
+++ src/components/Flashes/TheFlashes.vue
@@ -1,13 +1,11 @@
 <template>
   <section>
-    <transition-group>
-      <div
-        :key="i"
-        v-for="(flash, i) in flashes"
-      >
-        <Flash :flash="flash"></Flash>
-      </div>
-    </transition-group>
+    <div
+      :key="i"
+      v-for="(flash, i) in flashes"
+    >
+      <Flash :flash="flash"></Flash>
+    </div>
   </section>
 </template>
 
