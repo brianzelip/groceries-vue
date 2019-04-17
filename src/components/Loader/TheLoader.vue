49510bbd4179e54c0fc4a995c0bee5da2e7ae627
Brian Zelip <bzelip@gmail.com>
2019-04-17 08:18:57 -0400

    TheLoader: Add two animated elipses approaches

--- src/components/Loader/TheLoader.vue
+++ src/components/Loader/TheLoader.vue
@@ -1,23 +1,68 @@
 <template>
-  <div class="loader"></div>
+  <section class="flex justify-center">
+    <p class="loading h3">Fetching data&nbsp;</p>
+  </section>
 </template>
 
 <style>
-.loader {
-  border: 16px solid #f3f3f3; /* Light grey */
-  border-top: 16px solid #3498db; /* Blue */
-  border-radius: 50%;
-  width: 120px;
-  height: 120px;
-  animation: spin 2s linear infinite;
+/* via https://stackoverflow.com/a/54349616/2145103 */
+.loading:after {
+  content: "\00a0\00a0\00a0";
+  animation: progress-ellipsis 500ms infinite;
 }
 
-@keyframes spin {
+@keyframes progress-ellipsis {
   0% {
-    transform: rotate(0deg);
+    content: "\00a0\00a0\00a0";
   }
-  100% {
-    transform: rotate(360deg);
+  30% {
+    content: ".\00a0\00a0";
+  }
+  60% {
+    content: "..\00a0";
+  }
+  90% {
+    content: "...";
   }
 }
+
+/* 
+via https://codepen.io/thetallweeks/pen/yybGra
+
+.loading:after {
+  overflow: hidden;
+  display: inline-block;
+  vertical-align: bottom;
+  animation: ellipsis steps(4, end) 900ms infinite;
+  content: "\2026"; /* ascii code for the ellipsis character */ /*
+  width: 0px;
+}
+
+@keyframes ellipsis {
+  to {
+    width: 1.25em;
+  }
+}
+
+.loading:after {
+  content: "\00a0\00a0\00a0";
+  animation: progress-ellipsis 500ms infinite;
+}
+
+@keyframes progress-ellipsis {
+  0% {
+    content: "\00a0\00a0\00a0";
+  }
+  30% {
+    content: ".\00a0\00a0";
+  }
+  60% {
+    content: "..\00a0";
+  }
+  90% {
+    content: "...";
+  }
+}
+
+*/
 </style>
