22a49266399e860e26cfda39f892ec5e9153c197
Brian Zelip <bzelip@gmail.com>
2019-04-17 07:01:23 -0400

    TheGroceryForm: Register and conditionally render Loader

--- src/components/GroceryForm/TheGroceryForm.vue
+++ src/components/GroceryForm/TheGroceryForm.vue
@@ -1,5 +1,6 @@
 <template>
   <form method="POST">
+    <TheLoader v-if="showLoader"></TheLoader>
     <ol class="list-reset border rounded">
       <template v-for="item in allPossibleGroceryItems">
         <GroceryFormItem
@@ -15,20 +16,26 @@
 <script>
 import { mapState, mapActions } from "vuex";
 
+import TheLoader from "../Loader/TheLoader.vue";
 import GroceryFormItem from "./GroceryFormItem.vue";
 import TheGroceryFormControls from "./TheGroceryFormControls.vue";
 
 export default {
   props: ["flash"],
   components: {
+    TheLoader,
     GroceryFormItem,
     TheGroceryFormControls
   },
   computed: {
-    ...mapState(["allPossibleGroceryItems"])
+    ...mapState(["showLoader", "allPossibleGroceryItems"])
   },
   methods: {
-    ...mapActions(["setAllPossibleGroceryItems", "resetEmailTo"])
+    ...mapActions([
+      "setShowLoader",
+      "setAllPossibleGroceryItems",
+      "resetEmailTo"
+    ])
   },
   created() {
     this.setAllPossibleGroceryItems();
