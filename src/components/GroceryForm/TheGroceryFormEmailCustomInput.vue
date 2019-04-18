c289b60132b9831813c32e17b5dcaa4832854751
Brian Zelip <bzelip@gmail.com>
2019-04-18 10:23:23 -0400

    TheGroceryFormEmailCustomInput.vue: Create component

---
+++ src/components/GroceryForm/TheGroceryFormEmailCustomInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <label for="customEmail">send the list here:</label>
+    <input
+      class="field border"
+      id="customEmail"
+      placeholder="enter email"
+      type="text"
+      v-model="customEmail"
+    >
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      customEmail: ""
+    };
+  }
+};
+</script>
