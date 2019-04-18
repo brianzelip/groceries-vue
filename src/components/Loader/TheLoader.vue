80560194cb673ac52347d25c3526eb492c89adb3
Brian Zelip <bzelip@gmail.com>
2019-04-18 06:44:28 -0400

    TheLoader: Use cross platform loader

--- src/components/Loader/TheLoader.vue
+++ src/components/Loader/TheLoader.vue
@@ -1,68 +1,27 @@
 <template>
-  <section class="flex justify-center">
-    <p class="loading h3">Fetching data&nbsp;</p>
+  <section class="flex flex-column flex-center">
+    <span class="block loader"></span>
+    <p class="h3">Fetching data...</p>
   </section>
 </template>
 
 <style>
-/* via https://stackoverflow.com/a/54349616/2145103 */
-.loading:after {
-  content: "\00a0\00a0\00a0";
-  animation: progress-ellipsis 500ms infinite;
+/* via https://www.w3schools.com/howto/howto_css_loader.asp */
+.loader {
+  border: 0.75rem solid var(--grey);
+  border-top: 0.75rem solid var(--blue);
+  border-radius: 50%;
+  width: 100px;
+  height: 100px;
+  animation: spin 1.5s linear infinite;
 }
 
-@keyframes progress-ellipsis {
+@keyframes spin {
   0% {
-    content: "\00a0\00a0\00a0";
+    transform: rotate(0deg);
   }
-  30% {
-    content: ".\00a0\00a0";
-  }
-  60% {
-    content: "..\00a0";
-  }
-  90% {
-    content: "...";
+  100% {
+    transform: rotate(360deg);
   }
 }
-
-/* 
-via https://codepen.io/thetallweeks/pen/yybGra
-
-.loading:after {
-  overflow: hidden;
-  display: inline-block;
-  vertical-align: bottom;
-  animation: ellipsis steps(4, end) 900ms infinite;
-  content: "\2026"; /* ascii code for the ellipsis character */ /*
-  width: 0px;
-}
-
-@keyframes ellipsis {
-  to {
-    width: 1.25em;
-  }
-}
-
-.loading:after {
-  content: "\00a0\00a0\00a0";
-  animation: progress-ellipsis 500ms infinite;
-}
-
-@keyframes progress-ellipsis {
-  0% {
-    content: "\00a0\00a0\00a0";
-  }
-  30% {
-    content: ".\00a0\00a0";
-  }
-  60% {
-    content: "..\00a0";
-  }
-  90% {
-    content: "...";
-  }
-}
-
-*/
 </style>
